aebe6ec8cfa0be6cdfb4d30646f48e90f42d8e90
Daniel Jadon Ryngler <dryngler@cs.arizona.edu>
2021-12-01 19:16:17 -0700

    upadted appointment

--- sql/queries/add&update/addAppointment.sql
+++ sql/queries/add&update/addAppointment.sql
@@ -22,7 +22,16 @@ insert into Xact values (xact_seq.nextval, %s, %s);
 */
 insert into Permit values (permit_seq.nextval, %s, %s, %s);
 insert into License values (license_seq.nextval, %s, %s, %s);
-insert into StateId values (license_seq.nextval, %s, %s, %s);
+insert into Registration values (registration_seq.nextval, %s, %s, %s);
+insert into StateId values (stateid_seq.nextval, %s, %s, %s);
+
+/* need to check only 1 license and state id*/
+
+select cust_id from license
+where cust_id = cust_id;
+
+select cust_id from StateId
+where cust_id = cust_id;
 
 
 /* Delete Appointment */
